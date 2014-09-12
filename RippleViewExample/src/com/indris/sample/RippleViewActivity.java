0f27c31c987336361e064c192e5d94a022472ee3
siris_cac <siriscac@gmail.com>
2014-09-13 02:14:04 +0530

    Added Helper Class for attaching to any view

--- RippleViewExample/src/com/indris/sample/RippleViewActivity.java
+++ RippleViewExample/src/com/indris/sample/RippleViewActivity.java
@@ -1,49 +1,56 @@
 package com.indris.sample;
 
+import com.indris.material.RippleViewHelper;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
-
-import com.indris.material.RippleView;
-
 
 public class RippleViewActivity extends Activity {
 
-	RippleView mButton;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ripple_view);
-        mButton = (RippleView) findViewById(R.id.btn);
-        mButton.setOnClickListener(new View.OnClickListener() {
-			
+	Button mButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ripple_view);
+
+		getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+		mButton = (Button) findViewById(R.id.btn);
+		new RippleViewHelper(this, mButton).setRippleColor(Color.parseColor("#8181F7"), (float) 0.6);
+		
+		mButton.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(), "Ripples Yo! :D", Toast.LENGTH_LONG).show();
+				Toast.makeText(getApplicationContext(), "Ripples Yo! :D",
+						Toast.LENGTH_LONG).show();
 			}
 		});
-    }
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.ripple_view, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.ripple_view, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
